cb500cc2b12b1056acf3e6eae3a5143006dc39ad
Stefan Kolb <stefan-kolb@web.de>
2018-04-18 12:23:32 +0200

    Clarify \dots cop

--- src/main/java/texcop/cop/latex/TextInsideMathMode.java
+++ src/main/java/texcop/cop/latex/TextInsideMathMode.java
@@ -19,7 +19,7 @@ import texcop.cop.Offense;
 public class TextInsideMathMode implements FileTask {
     private static final String MESSAGE = "Use \\mathit, \\text or \\textnormal for multiletter words in math mode";
     private static final Pattern MATH_MODE = Pattern.compile("(?:\\$|\\\\\\(|\\\\\\[)(.*?)(?:\\$|\\\\\\)|\\\\\\])");
-    private static final Pattern MULTILETTER = Pattern.compile("\\\\[a-zA-z]+\\{.*?}|\\\\[a-zA-z]+|([a-zA-Z]{2,})");
+    private static final Pattern MULTI_LETTER = Pattern.compile("\\\\[a-zA-z]+\\{.*?}|\\\\[a-zA-z]+|([a-zA-Z]{2,})");
 
     private Config config;
 
@@ -51,7 +51,7 @@ public class TextInsideMathMode implements FileTask {
         Matcher matcher = MATH_MODE.matcher(line);
         while (matcher.find()) {
             for (int i = 1; i <= matcher.groupCount(); i++) {
-                Matcher words = MULTILETTER.matcher(matcher.group(i));
+                Matcher words = MULTI_LETTER.matcher(matcher.group(i));
                 while (words.find()) {
                     for (int j = 1; j <= words.groupCount(); j++) {
                         if (words.group(j) != null) {
